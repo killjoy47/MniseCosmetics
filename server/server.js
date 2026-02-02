76e7569d898321350112e3bd415bc1f8e067b809
killjoy47 <leonidasloutou@gmail.com>
2026-02-02 07:51:10 +0000

    fix: allow DELETE and PUT methods in CORS

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors({
     origin: "*",
-    methods: ["GET", "POST", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "x-role"],
     maxAge: 86400 // Cache OPTIONS for 24 hours to boost speed
 }));
